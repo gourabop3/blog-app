a4aa78c7f588a582029aea859165996d6619b13b
gourabop3 <gourabmullick200@gmail.com>
2025-06-21 23:43:57 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ import { useAuth } from "./context/AuthProvider";
 import { Toaster } from "react-hot-toast";
 import UpdateBlog from "./dashboard/UpdateBlog";
 import Detail from "./pages/Detail";
+import NotFound from "./pages/NotFound";
 
 function App() {
   const location = useLocation();
